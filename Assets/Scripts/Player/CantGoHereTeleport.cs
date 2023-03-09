7eedb3ab78dbfcdfde443347cd79b24a5e844750
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2023-03-09 18:12:26 -0500

    Updating Stairs

--- Assets/Scripts/Player/CantGoHereTeleport.cs
+++ Assets/Scripts/Player/CantGoHereTeleport.cs
@@ -14,19 +14,60 @@ public class CantGoHereTeleport : MonoBehaviour
 
     //[SerializeField] private Collider playerCollider;
 
+
+    // Animator Reference
+    [SerializeField] private Animator blink_Anim;
+    [SerializeField] private Animator blink_Anim_2;
+
+
+
     private void OnTriggerEnter(Collider other) 
     {
         if (other.tag == "Player")
         {
             trig = true;
+
+            blink_Anim.Play("TopLidBlink", 0, 0.25f);
+            blink_Anim_2.Play("BottomLidBlink", 0, 0.25f);
+            StartCoroutine(TransitionAfterBlink());
+
+
             // Debug.Log("Player Entered");
             // Vector3 offset = other.transform.position - transform.position;
             // other.transform.position = warpTarget.position + offset;
-
-            characterController.enabled = false;
-            player.transform.position = warpPoint.transform.position;
-            player.transform.rotation = warpPoint.transform.rotation;
-            characterController.enabled = true;
         }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        trig = false;    
+    }
+
+
+    // private void Update() 
+    // {
+    //     if (trig)
+    //     {
+ 
+    //     }    
+    // }
+
+
+    void StartBlinkAnimation()
+    {
+        blink_Anim.Play("TopLidBlink", 0, 0.25f);
+        blink_Anim_2.Play("BottomLidBlink", 0, 0.25f);
+    }
+
+    IEnumerator TransitionAfterBlink()
+    {
+        yield return new WaitForSeconds(0.40f);
+        
+        characterController.enabled = false;
+
+        player.transform.position = warpPoint.transform.position;
+        player.transform.rotation = warpPoint.transform.rotation;
+
+        characterController.enabled = true;
     }
 }
