6a13f0271644e18bfd00aa4861feef0cd226572a
Zane Brown <56931405+BeardedArtist@users.noreply.github.com>
2023-04-01 23:26:28 -0400

    Brightness Meter Fixed

--- Assets/Scripts/UI/Brightness.cs
+++ Assets/Scripts/UI/Brightness.cs
@@ -3,31 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Brightness : MonoBehaviour
 {
+    [SerializeField] private Volume volume;
+    private ColorAdjustments colorAdjustments;
     public Slider brightnessSlider;
 
-    public PostProcessProfile brightness;
-    public PostProcessLayer layer;
-
-    AutoExposure exposure;
-    // Start is called before the first frame update
-    void Start()
+    private void Start() 
     {
-        brightness.TryGetSettings(out exposure);
-        AdjustBrightness(brightnessSlider.value);
+        //volume = GetComponent<Volume>();
+        volume.profile.TryGet(out colorAdjustments);
+
+        Test(brightnessSlider.value);
+
     }
 
-    public void AdjustBrightness(float value)
+    public void Test(float value)
     {
         if (value != 0)
         {
-            exposure.keyValue.value = value;
+            colorAdjustments.postExposure.value = value;
         }
         else
         {
-            exposure.keyValue.value = .05f;
+            colorAdjustments.postExposure.value = 0f;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+    // --- ORIGINAL CODE ----
+
+
+    // public Slider brightnessSlider;
+
+    // public PostProcessProfile brightness;
+    // public PostProcessLayer layer;
+
+    // AutoExposure exposure;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     brightness.TryGetSettings(out exposure);
+    //     AdjustBrightness(brightnessSlider.value);
+    // }
+
+    // public void AdjustBrightness(float value)
+    // {
+    //     if (value != 0)
+    //     {
+    //         exposure.keyValue.value = value;
+    //     }
+    //     else
+    //     {
+    //         exposure.keyValue.value = .05f;
+    //     }
+    // }
 }
