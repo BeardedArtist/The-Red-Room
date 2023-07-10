9508d71007015790a30f04919d84428459a52cd4
Azmio <34865761+Azmio@users.noreply.github.com>
2023-07-10 21:25:45 +0200

    Working on Door Opening w/ lock

--- Assets/Scripts/Azmio Scripts/Lock.cs
+++ Assets/Scripts/Azmio Scripts/Lock.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class Lock : MonoBehaviour
 {
-    public InputField inputField;
+    public TMP_InputField inputField;
 
     const string correctCombination = "vile";
 
