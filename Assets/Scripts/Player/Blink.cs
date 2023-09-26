07e1b153f9f7f22780af6aee01abd1356140500f
Azmio <34865761+Azmio@users.noreply.github.com>
2023-09-25 22:53:41 -0600

    Blink Updated
    Blink now cannot be triggered when animation already playing

--- Assets/Scripts/Player/Blink.cs
+++ Assets/Scripts/Player/Blink.cs
@@ -9,49 +9,43 @@ public class Blink : MonoBehaviour
     [SerializeField] private Animator blink_Anim;
     [SerializeField] private Animator blink_Anim_2;
     [SerializeField] public bool isBlinking;
-    [SerializeField] private float timer;
-    float blinkTimer = 2f;
+    [SerializeField] private float randomBlinkTimer;
+    float blinkTimer = 1.1f;
 
     void Start() 
     {
-        timer = Random.Range(40.0f, 60.0f); 
+        randomBlinkTimer = Random.Range(40.0f, 60.0f);
     }
 
 
     void Update()
     {
-        timer -= Time.deltaTime;
+        randomBlinkTimer -= Time.deltaTime;
 
-        if (timer <= 0 && !isBlinking)
+        if ((randomBlinkTimer <= 0 || Input.GetMouseButton(1) && blinkTimer == 1.1f))
         {
-            timer = Random.Range(40.0f, 60.0f);
-            isBlinking = true;
+            randomBlinkTimer = Random.Range(40.0f, 60.0f);
+            StartBlink();
         }
 
-        if (Input.GetMouseButton(1) && !isBlinking)
+        if (isBlinking)
         {
-            isBlinking = true;
-        }
-        
-        if (isBlinking == true)
-        {
-            BlinkActive();
             blinkTimer -= Time.deltaTime;
-            // isBlinking is true and therefor activates the animation for the entire 2sec it's true
+        }
 
-            if(blinkTimer != 0)
-            {
-                isBlinking = false;
-                blinkTimer = 2f;
-            }
+        if(blinkTimer <= 0)
+        {
+            isBlinking = false;
+            blinkTimer = 1.1f;
         }
     }
 
 
-    void BlinkActive()
+    void StartBlink()
     {
+        isBlinking = true;
+
         blink_Anim.Play("TopLidBlink", 0, 0.25f);
         blink_Anim_2.Play("BottomLidBlink", 0, 0.25f);
-        Debug.Log("yeah");
     }
 }
