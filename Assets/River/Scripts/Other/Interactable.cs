b3e7eb5662b303bc596e80d03f3b9724a824a63d
Daxx <64298070+Daxxboii@users.noreply.github.com>
2024-02-09 02:47:16 +0530

    Dialogue System Basic

--- Assets/River/Scripts/Other/Interactable.cs
+++ Assets/River/Scripts/Other/Interactable.cs
@@ -5,25 +5,52 @@ using UnityEngine;
 using MyBox;
 public class Interactable : MonoBehaviour
 {
+    #region PreDefined
     // ReSharper disable once IdentifierTypo
     public enum InteractableType { FishBowl,BookShelf }
-
+    
     [Serializable]
     public struct Details
     {
-        public string Dialogue;
-        [Range(1, 3)] public int Day;
-        public AudioClip DialogueAudio;
+        [TextArea(1,5)]public List<string> Dialogue;
+        public List<AudioClip> DialogueAudio;
+        public List<string> Responses;
+        public List<AudioClip> ResponseClips;
+        public bool DisableAfterDialogue;
+        [Range(1f, 10f)] public float DisableDelay;
+        [Range(1, 4)] public int Day;
     }
 
+    #endregion
+    
     public InteractableType type;
 
     [Foldout("Interactable Details", true)] [SerializeField]
     public List<Details> AllDetails;
 
-
+    [Foldout("Debug",true)]
+    [Range(0f, 10f)] public float GizmoSize;
+    public Color GizmoColor;
+    
+    [ButtonMethod]
     public void Interact()
     {
-        
+        switch (type)
+        {
+            case InteractableType.BookShelf :
+                DialogueManager.instance.ShowDialogue("Player",AllDetails[0].Dialogue,AllDetails[0].DisableAfterDialogue,AllDetails[0].DisableDelay,AllDetails[0].DialogueAudio);
+                break;
+            case InteractableType.FishBowl :
+                DialogueManager.instance.ShowDialogue("Player",AllDetails[0].Dialogue,AllDetails[0].DisableAfterDialogue,AllDetails[0].DisableDelay,AllDetails[0].DialogueAudio);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = GizmoColor;
+        Gizmos.DrawSphere(transform.position, GizmoSize);
     }
 }
--- Assets/River/Scripts/UI/DialogueManager.cs
+++ Assets/River/Scripts/UI/DialogueManager.cs
@@ -1,14 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using Febucci.UI;
+using Febucci.UI.Core;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager instance;//Singleton
+    public AudioSource DialogueSource;
+    public TextAnimatorPlayer PlayerDialogue;
+    public TextMeshProUGUI PlayerName;
 
-    public TextMeshProUGUI Dialogue;
+    private bool DisableTextAfterShowing;
+    private float DisableDelay;
     private void Awake()
     {
         if (instance == null) instance = this;
@@ -18,4 +26,43 @@ public class DialogueManager : MonoBehaviour
             instance = this;
         }
     }
+    public void ShowDialogue(string Character,List<string> Dialogue,bool Disable,float DisableDelay,List<AudioClip> clip)
+    {
+        switch (Dialogue.Count)
+        {
+            case 1:
+                PlayerName.text = Character + ": ";
+                PlayerDialogue.ShowText( Dialogue[0]);
+                if(clip.Count>0)PlayAudioClip(clip[0]);
+                if (Disable) DOVirtual.Float(0, 1, DisableDelay, (value) => { }).OnComplete(() => { PlayerDialogue.ShowText(""); PlayerName.text = ""; });
+                break;
+            
+            case > 1:
+                var index = 0;
+                var nSeconds = 5; // Fading Timeout
+                PlayerName.text = Character + ": ";
+                PlayerDialogue.ShowText(Dialogue[index]);
+                DOVirtual.Float(0, 1, nSeconds, (value) => { }).SetLoops(Dialogue.Count-1).OnStepComplete(()=>{
+                    index++;
+                    PlayerDialogue.ShowText(Dialogue[index]);
+                }).OnComplete(() =>
+                {
+                    if (Disable) DOVirtual.Float(0, 1, DisableDelay, (value) => { }).OnComplete(() => { PlayerDialogue.ShowText(""); PlayerName.text = ""; });
+                });
+                break;
+            default:
+                Debug.LogError("No Dialogue Entered");
+                break;
+        }
+        
+    }
+    
+    private void PlayAudioClip(AudioClip clip)
+    {
+        if (clip == null) return;
+        DialogueSource.clip = clip;
+        DialogueSource.Play();
+    }
 }
+
+
